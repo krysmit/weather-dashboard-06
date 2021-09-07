b3d0414046f3c44d1f8507187e1a5e17fd8c9713
Krystyn Smith <krskrsmith@gmail.com>
2021-09-06 22:32:25 -0500

    I worked with my tutor and fixed the parts of my code preventing the button from working and the api pull from happening! We got the forcast to work, but I need to adjust the code to get the current weather to show up. Local storage is also in the works.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 console.log("hello I'm linked!");
-var $searchButton = $("#search-style");
-var $searchInput = $("#searchInput").val();
+var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || []
+// var $searchInput = $("#searchInput").val();
 var apiKey = "ad4e109e5b7a1f3554d123fbf819c27f";
 
 //panels to display information
@@ -18,49 +18,93 @@ function getPanelHTML(date, temp) {
 }
 
 //Function to save to local storage
-$("search-style").click(function (){
-    $searchInput = $("#searchInput").val();
-    var checkArray = searchHistory.inclues($searchInput);
+$("#search-style").click(function (event){
+    event.preventDefault()
+    let searchInput = $("#searchInput").val();
+    console.log(searchInput)
+    var checkArray = searchHistory.includes(searchInput);
 
-    if (checkArray == true) {
-        return;
-    } else {
-        searchHistory.push($searchInput);
+    if (checkArray === false) {
+        searchHistory.push(searchInput);
         localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
-    
+    }
+    searchButtonHandler(searchInput)
+    currentWeather(searchInput);
 //need to write in code to list out history items below search area
-}});
+});
 
-
+//api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
 
 //When the search button is clicked - API ajax
-function searchButtonHandler(e) {
-    e.preventDefault();
-    var searchInput = $searchInput;
+function searchButtonHandler(searchInput) {
+    // e.preventDefault();
+    // var searchInput = $searchInput;
     console.log(searchInput);
-    var weatherURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&appid" + apiKey;
-
+    var weatherURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&appid=" + apiKey+"&units=imperial";
+     console.log(weatherURL)
     $.ajax({
         //get data from api
         url: weatherURL,
-        succes: function (data) {
+        method:"GET"
+    }).then(function (data) {
             console.log(data);
             var appendedDates = [];
-            var appendCount = 0;
+            var appendHTML = 0;
 
-            for (let i=0; i < 5; i++) {
+            for (let i=0; i < data.list.length; i=i+8) {
                 var curr = data.list[i]
-                var currDateTexSt = curr.dt_txt
+                var currDateText = curr.dt_txt
                 var currDate = currDateText.split(" ")[0];
+                appendHTML += `<div class="card btn-primary">
+                <h2>${currDate}</h2>
+                <h3>Temp:${curr.main.temp}
+                <img src="https://openweathermap.org/img/wn/${curr.weather[0].icon}@2x.png" /></h3>
+                <h4>Description: ${curr.weather[0].description}</h4>
+                <h5>Humidity: ${curr.main.humidity}</h5>
+                <h6>Wind Speed: ${curr.wind.speed}</h6>
+
+                                `
 
                 console.log(currDate)
             }
 
-            $("#myWeather").append(
-                getPanelHTML(date, temp)
-            )
-        }
+            $("#fiveday").html(appendHTML)
+        
+    })   
+}
+//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+function currentWeather(searchInput) {
+    // e.preventDefault();
+    // var searchInput = $searchInput;
+    console.log(searchInput);
+    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&appid=" + apiKey+"&units=imperial";
+     console.log(weatherURL)
+    $.ajax({
+        //get data from api
+        url: weatherURL,
+        method:"GET"
+    }).then(function (data) {
+            console.log(data);
+            var appendedDates = [];
+            var appendHTML = 0;
+    console.log("it works")
+            
+                let = appendHTML += `<div class="card btn-primary">
+                <h2>${searchInput}</h2>
+                <h3>Temp:${data}
+                <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h3>
+                <h4>Description: ${data.weather[0].description}</h4>
+                <h5>Humidity: ${datamain.humidity}</h5>
+                <h6>Wind Speed: ${data.wind.speed}</h6>
+                                `
+
+                console.log(currDate)
+            
+
+            $("#fiveday").html(appendHTML)
+        
     })   
 }
 
-$searchButton.click(searchButtonHandler);
+
+// $searchButton.click(searchButtonHandler);
